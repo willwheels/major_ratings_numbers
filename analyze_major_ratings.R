076ef3ec53110f37f1839496fc2ffbcae273e234
willwheels <wheeler.william@epa.gov>
2020-10-06 19:31:09 -0400

    update programs

--- analyze_major_ratings.R
+++ analyze_major_ratings.R
@@ -4,29 +4,44 @@ library(pool)
 library(lubridate)
 library(ggplot2)
 
-print("are you connected to Pulse Secure or in the office?")
+# print("are you connected to Pulse Secure or in the office?")
+# 
+# source("./sources/db_connection.R")
+# 
+# dbpool <- pool::dbPool(odbc::odbc(),
+#                        dsn = db_connection$connect_name,
+#                        uid = db_connection$user_name,
+#                        pwd = db_connection$password
+# )
+# 
+# 
+# icis_permit <- dplyr::tbl(dbpool, dbplyr::ident_q('idea_icis.icis_permit')) %>%
+#   select(EXTERNAL_PERMIT_NMBR, VERSION_NMBR, PERMIT_TYPE_CODE, ISSUE_DATE,
+#          MAJOR_MINOR_STATUS_FLAG, MAJOR_RATING_NMBR,
+#          TOTAL_DESIGN_FLOW_NMBR, ACTUAL_AVERAGE_FLOW_NMBR) %>%
+#   mutate(MAJOR_MINOR_STATUS_FLAG =
+#            if_else(is.na(MAJOR_MINOR_STATUS_FLAG), "N", MAJOR_MINOR_STATUS_FLAG)) %>%
+#   collect()
 
-source("./sources/db_connection.R")
+#save(icis_permit, file = "icis_permit_internal.Rda")
 
-dbpool <- pool::dbPool(odbc::odbc(),
-                       dsn = db_connection$connect_name,
-                       uid = db_connection$user_name,
-                       pwd = db_connection$password
-)
+load("icis_permit_internal.Rda")
 
+# test_for_aaron <- icis_permit %>%
+#   group_by(EXTERNAL_PERMIT_NMBR) %>%
+#   filter(n() > 1) %>%
+#   summarize(num_status = n_distinct(MAJOR_MINOR_STATUS_FLAG))
 
-icis_permit <- dplyr::tbl(dbpool, dbplyr::ident_q('idea_icis.icis_permit')) %>%
-  select(EXTERNAL_PERMIT_NMBR, VERSION_NMBR, PERMIT_TYPE_CODE, ISSUE_DATE,
-         MAJOR_MINOR_STATUS_FLAG, MAJOR_RATING_NMBR, 
-         TOTAL_DESIGN_FLOW_NMBR, ACTUAL_AVERAGE_FLOW_NMBR) %>%
-  mutate(MAJOR_MINOR_STATUS_FLAG = 
-           if_else(is.na(MAJOR_MINOR_STATUS_FLAG), "N", MAJOR_MINOR_STATUS_FLAG)) %>%
-  collect()
+#get most recent major rating
 
 icis_permit_mrat <- icis_permit %>%
-  filter(VERSION_NMBR == 0) %>%
+  filter(!is.na(MAJOR_RATING_NMBR)) %>%
+  group_by(EXTERNAL_PERMIT_NMBR) %>%
+  arrange(EXTERNAL_PERMIT_NMBR, VERSION_NMBR) %>%
+  slice(1) %>%
   mutate(state = substr(EXTERNAL_PERMIT_NMBR, 1, 2)) 
 
+length(unique(icis_permit$EXTERNAL_PERMIT_NMBR))
 
 ggplot(data = icis_permit_mrat %>% 
          filter(!is.na(MAJOR_RATING_NMBR)),
@@ -37,68 +52,32 @@ ggplot(data = icis_permit_mrat %>%
 ggplot(data = icis_permit_mrat %>% 
          filter(!is.na(MAJOR_RATING_NMBR), MAJOR_RATING_NMBR > 0),
        aes(x = MAJOR_RATING_NMBR )) +
-  geom_histogram(binwidth = 5, boundary = 0) +
+  geom_histogram(binwidth = 5) +
   theme_minimal()
 
 
 ggplot(data = icis_permit_mrat %>% 
          filter(!is.na(MAJOR_RATING_NMBR), MAJOR_RATING_NMBR > 0, MAJOR_RATING_NMBR <200),
        aes(x = MAJOR_RATING_NMBR )) +
-  geom_histogram(binwidth = 5, boundary = 0, closed = "left") +
+  geom_histogram(binwidth = 5) +
   geom_vline(xintercept = 80, linetype = 2) +
   theme_minimal()
 
-ggplot(data = icis_permit_mrat %>% 
-         filter(!is.na(MAJOR_RATING_NMBR), MAJOR_RATING_NMBR > 0, MAJOR_RATING_NMBR <200),
-       aes(x = MAJOR_RATING_NMBR )) +
-  geom_histogram(binwidth = 1, boundary = 0, closed = "left") +
-  geom_vline(xintercept = 80, linetype = 2) +
-  theme_minimal()
 
-test1 <- icis_permit_mrat %>% 
-  filter(MAJOR_RATING_NMBR >0, !is.na(MAJOR_RATING_NMBR))
-
-test2 <- icis_permit_mrat %>% 
-  filter(MAJOR_RATING_NMBR >0, !is.na(MAJOR_RATING_NMBR)) %>%
-  group_by(state) %>%
-  mutate(count = n())
-
-unique(test2$state)
-
-count_by_state <- test2 %>%
+count_by_state <- icis_permit_mrat %>%
   group_by(state) %>%
   count(sort = TRUE)
 
-iowa <- test2 %>%
+iowa <- icis_permit_mrat %>%
   filter(state == "IA") %>%
   group_by(MAJOR_RATING_NMBR) %>%
   count()
 
 
-ggplot(data = test2 %>% 
-         filter(!(state == "IA"), count > 29, MAJOR_RATING_NMBR <100),
+ggplot(data = icis_permit_mrat %>% filter(state == "KY", MAJOR_RATING_NMBR > 0, MAJOR_RATING_NMBR <100),
        aes(x = MAJOR_RATING_NMBR)) +
-  geom_histogram(binwidth = 5, boundary = 0, closed = "left") +
-  geom_vline(xintercept = 80, linetype = 2) +
-  theme_minimal() +
-  facet_wrap(~ state)
-  
-ggplot(data = test2 %>% 
-        filter(state == "TX", MAJOR_RATING_NMBR <200),
-      aes(x = MAJOR_RATING_NMBR)) +
-  geom_histogram(binwidth = 5, boundary = 0, closed = "left") +
+  geom_histogram(binwidth = 5) +
   geom_vline(xintercept = 80, linetype = 2) +
   theme_minimal() 
 
-
-percent_reporting <- icis_permit_mrat %>%
-  filter(PERMIT_TYPE_CODE == "NPD") %>%
-  mutate(mrat_reported = !is.na(MAJOR_RATING_NMBR)) %>%
-  group_by(state) %>%
-  summarise(total = n(), 
-            mrat_reported = sum(mrat_reported),
-            percent_reported = mrat_reported/total) %>%
-  ungroup() %>%
-  arrange(desc(percent_reported))
   
-
